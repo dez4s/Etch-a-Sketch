874822775f61d82cdd072cde9e13917d5a916737
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-07-07 02:27:00 +0300

    Make grid flexible to increasing/decreasing nr of elements inside

--- javascript.js
+++ javascript.js
@@ -1,10 +1,17 @@
 const container = document.querySelector('.container');
 
-for(i = 0; i < 16*16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('boxes');
-    container.appendChild(div);
+for (i = 0; i < 16; i++) {
+    const line = document.createElement('div');
+    line.classList.add('lines');
+    container.appendChild(line);
+    for (j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        div.classList.add('boxes');
+        line.appendChild(div);
+    }
 }
+
+
 
 const grid = Array.from(container.children);
 
