d38dbe8c2d6bad8feaad8c5bb446c11082d9ef2f
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-07-07 14:53:00 +0300

    Add a button that takes a number as input and generates a new grid

--- javascript.js
+++ javascript.js
@@ -1,26 +1,38 @@
 const container = document.querySelector('.container');
+const btn = document.querySelector('#size');
+let gridSize = 16;
 
-for (i = 0; i < 16; i++) {
-    const line = document.createElement('div');
-    line.classList.add('lines');
-    container.appendChild(line);
-    for (j = 0; j < 16; j++) {
-        const div = document.createElement('div');
-        div.classList.add('boxes');
-        line.appendChild(div);
+function createGrid(size) {
+    for (i = 0; i < size; i++) {
+        const line = document.createElement('div');
+        line.classList.add('lines');
+        document.querySelector('.container').appendChild(line);
+        for (j = 0; j < size; j++) {
+            const div = document.createElement('div');
+            div.classList.add('boxes');
+            line.appendChild(div);
+        }
     }
+    const grid = document.querySelectorAll('.boxes');
+
+    grid.forEach(box => {
+        box.addEventListener('mouseenter', changeColor);
+    });
+
+    function changeColor(e) {
+        if (e.buttons == 1) e.target.classList.add('red');
+        e.preventDefault();
+    }   
 }
 
+createGrid(gridSize);
 
-
-const grid = Array.from(container.children);
-
-grid.forEach(box => {
-    box.addEventListener('mouseenter', changeColor);
+btn.addEventListener('click', e => {
+    inputNumber = Number(prompt('Please enter a new grid size', 16));
+    inputNumber ? gridSize = inputNumber : alert('Please enter a valid number');
+    const newContainer = document.createElement('div');
+    document.body.replaceChild(newContainer, document.querySelector('.container'));
+    newContainer.classList.add('container');
+    createGrid(gridSize);
 });
-
-function changeColor(e) {
-    console.log(this);
-    e.target.classList.add('red');
-}
 
