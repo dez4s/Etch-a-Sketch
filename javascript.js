0ff9738de2ccfccafaa7293f699487d9f0668bea
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2023-07-09 03:09:47 +0300

    Implement a color randomizer mode

--- javascript.js
+++ javascript.js
@@ -2,10 +2,21 @@ const container = document.querySelector('.container');
 const slider = document.querySelector('#size');
 const paraSize = document.querySelector('.range-number');
 const colorPicker = document.querySelector('input[type="color"]');
+const clearBtn = document.querySelector('.reset');
+const randomBtn = document.querySelector('.random-container > button');
+randomBtn.classList.add('noRainbow');
+const blackBtn = document.querySelector('.black');
+blackBtn.classList.add('noFadeBlack');
+const colorContainer = document.querySelector('.color-container');
+
+const text = document.createElement('p');
+text.textContent = 'Disabled';
+
 
 let gridSize = 16; // default gridSize
 let colorHolder = 'black'; //default color
 paraSize.textContent = `${gridSize}`;
+
 
 function createGrid(size) {
     for (i = 0; i < size; i++) {
@@ -18,7 +29,6 @@ function createGrid(size) {
             line.appendChild(div);
         }
     } // create i lines with j divs on each line
-
     const grid = document.querySelectorAll('.boxes');
 
     grid.forEach(box => {
@@ -26,8 +36,17 @@ function createGrid(size) {
     }); // change color on mouseenter event
 }
 
+
 function changeColor(e) {
-    if (e.buttons == 1) e.target.style.backgroundColor = colorHolder;
+    if (e.buttons == 1 && !rainbowOn && !fadeBlack) { 
+        e.target.style.backgroundColor = colorHolder;
+    } else if (e.buttons == 1 && rainbowOn) {
+        colorHolder = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+        e.target.style.backgroundColor = colorHolder;
+    } else if (e.buttons == 1 && fadeBlack) {
+        colorHolder = 'grey';
+        e.target.style.backgroundColor = colorHolder;
+    }
     e.preventDefault();
 }   
 
@@ -45,7 +64,52 @@ slider.addEventListener('change', e => {
     createGrid(gridSize);
 });
 
-
 colorPicker.addEventListener('input', e => {
     colorHolder = e.target.value;  
 });
+
+clearBtn.addEventListener('click', () => {
+    const newContainer = document.createElement('div');
+    document.querySelector('.big-container').replaceChild(newContainer, document.querySelector('.container'));
+    newContainer.classList.add('container');
+    createGrid(gridSize);
+});
+
+let rainbowOn = 0;
+
+randomBtn.addEventListener('click', (e) => {
+    fadeBlack = 0;
+    blackBtn.classList.replace('fadeBlack', 'noFadeBlack');
+    if (!rainbowOn) {
+        rainbowOn = 1;
+        e.currentTarget.classList.replace('noRainbow', 'rainbow');      
+        if (Array.from(colorContainer.children).includes(colorPicker)) {
+            colorContainer.replaceChild(text, colorPicker);
+        }
+    } else {
+        e.currentTarget.classList.replace('rainbow', 'noRainbow');
+        rainbowOn = 0;
+        colorContainer.replaceChild(colorPicker, text);
+        colorHolder = colorPicker.value;
+    }
+});
+
+let fadeBlack = 0;
+
+blackBtn.addEventListener('click', (e) => {
+    rainbowOn = 0;
+    randomBtn.classList.replace('rainbow', 'noRainbow');
+    if (!fadeBlack) {
+        fadeBlack = 1;
+        e.currentTarget.classList.replace('noFadeBlack', 'fadeBlack');      
+        if (Array.from(colorContainer.children).includes(colorPicker)) {
+            colorContainer.replaceChild(text, colorPicker);
+        }
+    } else {
+        e.currentTarget.classList.replace('fadeBlack', 'noFadeBlack');
+        fadeBlack = 0;
+        colorContainer.replaceChild(colorPicker, text);
+        colorHolder = colorPicker.value;
+    }
+});
+
